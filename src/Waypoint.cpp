3e313be5d30147b86f8ca0ed481e7cee4435e848
Jonathan Ohayon <me@jonathano.com>
2019-01-18 16:41:23 +0200

    Waypoint -> trail::Waypoint

--- src/Waypoint.cpp
+++ src/Waypoint.cpp
@@ -4,33 +4,33 @@
 
 #include "Waypoint.h"
 
-Waypoint::Waypoint(double x, double y, double theta) {
+trail::Waypoint::Waypoint(double x, double y, double theta) {
     this->mX = x;
     this->mY = y;
     this->mTheta = radians(90 - theta);
 }
 
-double Waypoint::getX() {
+double trail::Waypoint::getX() {
     return this->mX;
 }
 
-double Waypoint::getY() {
+double trail::Waypoint::getY() {
     return this->mY;
 }
 
-Eigen::Vector2d Waypoint::firstDerivative(double scale) {
+Eigen::Vector2d trail::Waypoint::firstDerivative(double scale) {
     Eigen::Vector2d vec;
     vec << cos(this->mTheta), sin(this->mTheta);
     return scale * vec;
 }
 
-Eigen::Vector2d Waypoint::secondDerivative(double scale) {
+Eigen::Vector2d trail::Waypoint::secondDerivative(double scale) {
     Eigen::Vector2d vec;
     vec << -sin(this->mTheta), cos(this->mTheta);
     return scale * vec;
 }
 
-double Waypoint::distanceToPoint(Waypoint otherPoint) {
+double trail::Waypoint::distanceToPoint(trail::Waypoint otherPoint) {
     double dx = otherPoint.getX() - this->mX;
     double dy = otherPoint.getY() - this->mY;
     return hypot(dx, dy);
--- src/Waypoint.h
+++ src/Waypoint.h
@@ -3,52 +3,54 @@
 
 #include <Eigen/Dense>
 
-class Waypoint {
-private:
-    double mX;
-    double mY;
-    double mTheta;
+namespace trail {
+    class Waypoint {
+    private:
+        double mX;
+        double mY;
+        double mTheta;
 
-public:
-    /**
-     * Creates a new Waypoint instance.
-     * @param x The x coordinate of the robot's position
-     * @param y The y coordinate of the robot's position
-     * @param theta The robot's angle, in degrees
-     */
-    Waypoint(double x, double y, double theta);
-    ~Waypoint() = default;
+    public:
+        /**
+         * Creates a new Waypoint instance.
+         * @param x The x coordinate of the robot's position
+         * @param y The y coordinate of the robot's position
+         * @param theta The robot's angle, in degrees
+         */
+        Waypoint(double x, double y, double theta);
+        ~Waypoint() = default;
 
-    /**
-     * @return The x coordinate of the selected Waypoint
-     */
-    double getX();
+        /**
+         * @return The x coordinate of the selected Waypoint
+         */
+        double getX();
 
-    /**
-     * @return The y coordinate of the selected Waypoint
-     */
-    double getY();
+        /**
+         * @return The y coordinate of the selected Waypoint
+         */
+        double getY();
 
-    /**
-     * Calculates the required first derivative in order to keep the spline in the desired angle.
-     * @param scale A parameter to scale the vector with. Default = 1.75
-     * @return A scaled unit vector with the angle given to the constructor
-     */
-    Eigen::Vector2d firstDerivative(double scale = 1.75);
+        /**
+         * Calculates the required first derivative in order to keep the spline in the desired angle.
+         * @param scale A parameter to scale the vector with. Default = 1.75
+         * @return A scaled unit vector with the angle given to the constructor
+         */
+        Eigen::Vector2d firstDerivative(double scale = 1.75);
 
-    /**
-     * Calculates the required second derivative in order to keep the spline in the desired angle.
-     * @param scale A parameter to scale the vector with. Default = 0.15
-     * @return A scaled unit vector with the angle given to the constructor
-     */
-    Eigen::Vector2d secondDerivative(double scale = 0.15);
+        /**
+         * Calculates the required second derivative in order to keep the spline in the desired angle.
+         * @param scale A parameter to scale the vector with. Default = 0.15
+         * @return A scaled unit vector with the angle given to the constructor
+         */
+        Eigen::Vector2d secondDerivative(double scale = 0.15);
 
-    /**
-     * Calculates the distance between the current Waypoint and a given Waypoint.
-     * @param otherPoint The Waypoint to calculate the distance to
-     * @return The distance, calculated using the Pythagorean theorem
-     */
-    double distanceToPoint(Waypoint otherPoint);
-};
+        /**
+         * Calculates the distance between the current Waypoint and a given Waypoint.
+         * @param otherPoint The Waypoint to calculate the distance to
+         * @return The distance, calculated using the Pythagorean theorem
+         */
+        double distanceToPoint(trail::Waypoint otherPoint);
+    };
+}
 
 #endif //TRAIL_WAYPOINT_H
