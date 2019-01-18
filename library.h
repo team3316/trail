67d9bbd97b1b8688f0e9b1d218c6ebf4e56648f5
Jonathan Ohayon <me@jonathano.com>
2019-01-18 13:56:54 +0200

    Documentation in library.h

--- library.h
+++ library.h
@@ -13,6 +13,17 @@ namespace trail {
         int mNumOfDriveMotors;
 
     public:
+        /**
+         * Creates a new Robot instance. At the moment this is only configured to work with transmissions that have only
+         * one type of motors connected to them (we use the VEXpro SSDR with 2 CIMs)
+         * @param mass The mass of the robot. Units - kg
+         * @param baseWidth The wheelbase width of the robot. Units - m
+         * @param freeSpeed The free speed of the robot (of one side). Units - m/s
+         * @param stallTorque The stall torque of a single motor connected to the transmission. Units - N*m
+         * @param gearRatio The final (input to output, not stages) gear ratio of a transmission.
+         * @param wheelRadius The drivetrain's wheel radius (provided that all of the wheels have a single radius). Units - m
+         * @param numOfDriveMotors The number of total drive motors (number of motors on the drivetrain)
+         */
         Robot(
                 double mass,
                 double baseWidth,
@@ -23,8 +34,30 @@ namespace trail {
                 int numOfDriveMotors
         );
 
-        double timeToMaxVelocity(double i = 5);
-        double distanceToMaxVelocity();
+        /**
+         * Calculates the time required to get the robot to 99.75% of its free speed, based on solving the ODE
+         * given here: https://drive.google.com/file/d/0B_lA0xR4_viqbTJYanE4X3VnWE0/view [2] and calculating its
+         * time constant.
+         * @param i The number to multiply the time constant in. 5 results in 99.32% of free speed, 4 (default) is 98.17%.
+         * @return The time to reach 98.17% of the robot's free speed using full power, in seconds.
+         */
+        double timeToMaxVelocity(double i = 4);
+
+        /**
+         * Calculates the needed distance required for the robot to reach end_vel in maximum acceleration. Calculated using
+         * the kinematics formula:
+         *     end_vel^2 = start_vel^2 + 2 * acceleration * delta_x
+         * @param startVel The starting velocity.
+         * @param endVel The end velocity.
+         * @return The required distance.
+         */
+        double distanceToMaxVelocity(double startVel = 0, double endVel = 0);
+
+        /**
+         * Calculates the maximum acceleration of the robot using Newton's 2nd law.
+         * F_max = ma ==> a_max = F_max / m
+         * @return The maximum acceleration
+        */
         double maxAcceleration();
     };
 }
